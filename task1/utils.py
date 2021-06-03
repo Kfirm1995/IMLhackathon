cb61937e2f80e2e3a456328ac567a2dc64bac0ec
Kfir Mizrahi <kfir.mizrahi@mail.huji.ac.il>
2021-06-03 14:43:08 +0300

    add top_5_languages

--- task1/utils.py
+++ task1/utils.py
@@ -4,8 +4,6 @@ import pandas as pd
 
 def plot_corr_heatmap(df):
     corr = df.corr()
-    print(corr)
-    print(corr['budget/runtime'])
     fig = plt.figure(figsize=(12, 12))
     c = plt.pcolor(corr, cmap='RdBu', vmin=-1, vmax=1)
     plt.xticks(np.arange(0.5, len(corr.columns), 1), corr.columns, rotation=40)
@@ -16,3 +14,4 @@ def plot_corr_heatmap(df):
 
 def get_top_n_freq_values(df:pd.DataFrame, n: int, feature: str):
     return df[feature].value_counts()[:n].index.tolist()
+
